5aef05382c7c2526e0f9fad8b4a7e5b1ecac8f0f
supallenn <bruhitsallen@yahoo.com>
2021-01-08 01:19:15 -0800

    Delete data.py

--- data.py
+++
@@ -1,6 +0,0 @@
-zoom = [
-    ["Zoom-URL(1)", "Start-Time", "End-Time", "Password", "Message-5-Min-After-Start-Time"],
-    ["Zoom-URL(2)", "Start-Time", "End-Time", "Password", "Message-5-Min-After-Start-Time"],
-    ["Zoom-URL(3)", "Start-Time", "End-Time", "Password", "Message-5-Min-After-Start-Time"],
-    ["Zoom-URL(4)", "Start-Time", "End-Time", "Password", "Message-5-Min-After-Start-Time"]
-]
